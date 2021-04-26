8d7be92d712af7386cf58c8ace98dada4ca65d9b
ivanpras05 <83106743+ivanpras05@users.noreply.github.com>
2021-04-26 13:34:43 +0700

    Add files via upload

---
+++ scr/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './App.css';
+import Dasboard from './Containers/Pages/Dasboard';
+//import MyRouter from './Configs/Routes';
+import { Router } from 'react-router-dom';
+function App() {
+  return ( <Dasboard/> );
+}
+
+export default App;
